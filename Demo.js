a656f310c8f5ccead37448da2898dc655aa642fb
Jaimin2499 <58000318+Jaimin2499@users.noreply.github.com>
2021-05-07 17:07:55 +0530

    Animation using javascript

---
+++ Demo.js
@@ -0,0 +1,20 @@
+function img1() {
+    document.getElementById('trip').src = "https://bestanimations.com/media/horses/748792066animated-horse-gif-88.gif";
+    document.getElementById('trip').style.position = "absolute"
+    document.getElementById('trip').style.top = "5%"
+    document.getElementById('trip').style.left = "52%"
+    document.getElementById('trip').style.transition = "3s"
+
+}
+
+function img2() {
+    document.getElementById('trip').src = "https://huddersfieldpetanque.files.wordpress.com/2018/02/drawn-horse-running-horse-16.gif";
+    document.getElementById('trip').style.width = "200px"
+    document.getElementById('trip').style.height = "200px"
+    document.getElementById('trip').style.borderRadius = "10px"
+    document.getElementById('trip').style.position = "absolute"
+    document.getElementById('trip').style.top = "2%"
+    document.getElementById('trip').style.left = "0%"
+    document.getElementById('trip').style.transition = "3s"
+
+}
